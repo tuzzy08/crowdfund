73eef53854d135acbc48574e175ed8eab2249cf8
Sage <tuzzy08@gmail.com>
2022-01-28 14:20:00 +0100

    Progress on project detail page

--- pages/[projectID].tsx
+++ pages/[projectID].tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Container, Flex, Heading, HStack, Image, VStack, SimpleGrid, Spacer, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Container, Flex, Heading, HStack, Image, VStack, SimpleGrid, Spacer, Stack, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import Layout from '../components/Layouts/Layout';
 
@@ -25,9 +25,30 @@ export default function ProjectDetails(props: any) {
 					<Flex border={'1px'}>
 						<Image border={'1px'} src='/prj3.png' alt='Dan Abramov' />
 					</Flex>
-					<Flex border={'1px'} w={'420px'} h={'520.500px'} justifyContent={'center'}>
+					<VStack border={'1px'} w={'420px'} h={'520.500px'} spacing={5}>
 						<Box width='95%' height='10px' bgColor={'red.400'} />
-					</Flex>
+						{/* <VStack spacing={5}> */}
+						<Flex direction={'column'}>
+							<Heading as='h1' fontSize='3xl' border={'1px'} color={'red.400'}>
+								MATIC: Project Balance
+							</Heading>
+							<Text>Remaining of goal</Text>
+							<Text fontSize='3xl' alignSelf={'flex-start'} mt={5}>
+								{' '}
+								Number of funders
+							</Text>
+							<Text fontSize='3xl' alignSelf={'flex-start'} mt={5}>
+								{' '}
+								Remaining time
+							</Text>
+						</Flex>
+
+						<Stack></Stack>
+						<Flex direction={'column'}></Flex>
+						{/* </VStack> */}
+
+						<Button></Button>
+					</VStack>
 				</Flex>
 			</VStack>
 		</Box>
