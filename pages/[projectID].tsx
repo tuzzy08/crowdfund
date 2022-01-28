ac9df8aebb3b7c863d2ecab465275f19e35b239a
Sage <tuzzy08@gmail.com>
2022-01-28 14:31:27 +0100

    Added more components to details page

--- pages/[projectID].tsx
+++ pages/[projectID].tsx
@@ -1,5 +1,13 @@
-import { Box, Button, Center, Container, Flex, Heading, HStack, Image, VStack, SimpleGrid, Spacer, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Container, Flex, Heading, HStack, Icon, Image, VStack, SimpleGrid, Spacer, Stack, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import {
+	IoAnalyticsSharp,
+	IoLogoBitcoin,
+	IoPulseSharp,
+	IoDesktop,
+	IoRestaurantOutline,
+	IoImagesOutline,
+} from 'react-icons/io5';
 import Layout from '../components/Layouts/Layout';
 
 export default function ProjectDetails(props: any) {
@@ -25,29 +33,35 @@ export default function ProjectDetails(props: any) {
 					<Flex border={'1px'}>
 						<Image border={'1px'} src='/prj3.png' alt='Dan Abramov' />
 					</Flex>
-					<VStack border={'1px'} w={'420px'} h={'520.500px'} spacing={5}>
+					<VStack
+						border={'1px'}
+						w={'420px'}
+						h={'520.500px'}
+						spacing={8}
+						pl={3}
+						pr={3}
+					>
 						<Box width='95%' height='10px' bgColor={'red.400'} />
-						{/* <VStack spacing={5}> */}
 						<Flex direction={'column'}>
 							<Heading as='h1' fontSize='3xl' border={'1px'} color={'red.400'}>
 								MATIC: Project Balance
 							</Heading>
 							<Text>Remaining of goal</Text>
-							<Text fontSize='3xl' alignSelf={'flex-start'} mt={5}>
+							<Text fontSize='3xl' alignSelf={'flex-start'} mt={10}>
 								{' '}
 								Number of funders
 							</Text>
-							<Text fontSize='3xl' alignSelf={'flex-start'} mt={5}>
+							<Text fontSize='3xl' alignSelf={'flex-start'} mt={10} mb={10}>
 								{' '}
 								Remaining time
 							</Text>
 						</Flex>
-
-						<Stack></Stack>
-						<Flex direction={'column'}></Flex>
-						{/* </VStack> */}
-
-						<Button></Button>
+						<Button width={'90%'}>Fund project</Button>
+						<HStack spacing={5}>
+							<Icon as={IoRestaurantOutline} color={'green.500'} w={5} h={5} />
+							<Icon as={IoRestaurantOutline} color={'green.500'} w={5} h={5} />
+							<Icon as={IoRestaurantOutline} color={'green.500'} w={5} h={5} />
+						</HStack>
 					</VStack>
 				</Flex>
 			</VStack>
