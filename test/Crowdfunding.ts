fbed2f986365c1e233227fa7c15243c607c14f25
Sage <tuzzy08@gmail.com>
2022-02-05 13:55:58 +0100

    Added more tests

--- test/Crowdfunding.ts
+++ test/Crowdfunding.ts
@@ -12,16 +12,31 @@ describe('Testing Crowdfunding contract', () => {
   // Local variables
   let CrowdfundingContractFactory;
   let CrowdfundingContract: Crowdfunding;
-  let TokenFactory;
+  let TokenContractAddress: string;
   let TokenContract: CrowdToken;
 	let owner: SignerWithAddress;
 	let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
 
+  beforeEach(async () => {
+    CrowdfundingContractFactory = await ethers.getContractFactory('Crowdfunding');
+    [owner, addr1, addr2] = await ethers.getSigners();
+    // Deploy contract which in turn deploys Token contract
+    CrowdfundingContract = await CrowdfundingContractFactory.deploy();
+    // Get Token contract address
+    TokenContractAddress = await CrowdfundingContract.getTokenContractAddress();
+  });
   
 
   describe('Testing deployment', () => {
-
+    it('Should be the owner of token contract', async () => { 
+      // Get Token contract instance
+       TokenContract = await ethers.getContractAt(
+					'CrowdToken',
+					TokenContractAddress
+				);
+      expect(CrowdfundingContract.address).to.equal(await TokenContract.owner());
+    });
   })
   
 
